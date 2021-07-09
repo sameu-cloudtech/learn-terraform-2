93c11d0702d8905fdde1fab94083721010fa22dd
sameu-cloudtech <70744023+sameu-cloudtech@users.noreply.github.com>
2021-07-09 11:58:42 +0800

    Add files via upload
    New

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "compartment_ocid" {
+	default = "ocid1.compartment.oc1..aaaaaaaau337m2maguz3zrssc4ie3jfkyfxalbpmgu2wglo26l7dg3nxmyaa"
+}
+
+variable "region" {
+	default = "ap-sydney-1"
+}
+
+variable "InstanceImageOCID" {
+  default = {
+    // See https://docs.cloud.oracle.com/images/
+    // Platform image "Autonomous Linux"
+    "ap-sydney-1" = "ocid1.image.oc1.ap-sydney-1.aaaaaaaaaba6jlgscasd7wdg6ly3z73dqzjxgrh5ubpbsbp2fnjl53pkrvnq"
+  }
+}
+
+
+variable "ssh_public_key" {
+	default = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBOIhBTtoVfQL1x9d3JDgDAm4oen9Cy/f1FOsIDcpPQTImsHyjyd35dZGY0sihS5qF+AHbjXkuQKnf5Yo0mtszdukzB9lsFxgR6C8e8N55X3v1LhJZJkxJT1M+/LEewjiq40LHOWr5jannn/CJ4srJfVBlxTMX5AOCdi5vXt2ncNLNjW0iJ1U0q/g9PhqkGthPHzgHEMi3FuH4ttiXpAHlFPPxdqmP8EDC8D+U2HKj1siJs4ohglXdP/o32DkegNJugCU5t7JWVbC6f0BGvQEt2OFQLykWqv/mcXVKOOvUfxfRmqRBYw7TT0zHNH2dcZyS4Is4D58GVbGIQBVkEf5T/ rsa-key-20191120"
+}
+
+# Defines the number of instances to deploy
+variable "NumInstances" {
+    default = "1"
+}
+
+variable "InstanceShape" {
+    default = "VM.Standard2.1"
+}
+
+# Specifies the Availability Domain
+variable "localAD" {
+    default = "rrHb:AP-SYDNEY-1-AD-1"
+}
---
+++ vcn.tf
@@ -0,0 +1,42 @@
+provider "oci" {
+  region = "${var.region}"
+}
+
+
+
+resource "oci_core_virtual_network" "vcndemo" {
+  cidr_block = "10.0.0.0/16"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "vcndemo"
+  dns_label = "vcndemo"
+}
+
+
+resource "oci_core_internet_gateway" "ExampleIG" {
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "TFExampleIG"
+  vcn_id = "${oci_core_virtual_network.vcndemo.id}"
+}
+
+
+resource "oci_core_route_table" "ExampleRT" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id = "${oci_core_virtual_network.vcndemo.id}"
+  display_name = "TFExampleRouteTable"
+  route_rules {
+    cidr_block = "0.0.0.0/0"
+    network_entity_id = "${oci_core_internet_gateway.ExampleIG.id}"
+  }
+}
+
+resource "oci_core_subnet" "ExampleSubnet" {
+  availability_domain = "${var.localAD}"
+  cidr_block = "10.0.1.0/24"
+  display_name = "ExampleSubnet"
+  dns_label = "examplesubnet"
+  security_list_ids = ["${oci_core_virtual_network.vcndemo.default_security_list_id}"]
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id = "${oci_core_virtual_network.vcndemo.id}"
+  route_table_id = "${oci_core_route_table.ExampleRT.id}"
+  dhcp_options_id = "${oci_core_virtual_network.vcndemo.default_dhcp_options_id}"
+}
